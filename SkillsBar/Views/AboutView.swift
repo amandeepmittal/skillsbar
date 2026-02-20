8da782d1d676857dd1ea0064fac3913218611762
Aman Mittal <amandeepmittal@live.com>
2026-02-20 13:51:14 +0530

    Bump version to 1.3.0

--- SkillsBar/Views/AboutView.swift
+++ SkillsBar/Views/AboutView.swift
@@ -40,7 +40,7 @@ struct AboutView: View {
                 VStack(spacing: 4) {
                     Text("SkillsBar")
                         .font(.system(size: 22, weight: .bold))
-                    Text("Version 1.2.5")
+                    Text("Version 1.3.0")
                         .font(.system(size: 13))
                         .foregroundStyle(.secondary)
                 }
